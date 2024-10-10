581fe5ba305eff0a5fd2421b011234bb71bd5dc1
watch you <76858251+Happynewyear308@users.noreply.github.com>
2024-10-10 18:07:21 +0800

    Add files via upload

---
+++ iconbutton.cpp
@@ -0,0 +1,47 @@
+﻿#include "iconbutton.h"
+#include <QDebug>
+
+IconButton::IconButton(QWidget *parent)
+    :QPushButton(parent){
+    // 设置初始图标
+//    setIcon(QIcon(":/image/show_label.png"));
+//    setFixedSize(parent->width(), parent->height());  // 设置图标大小
+    ischecked = true;
+}
+
+IconButton::~IconButton() {
+
+}
+
+void IconButton::mousePressEvent(QMouseEvent *event) {
+    if(event->button() == Qt::LeftButton) {
+        ischecked = !ischecked;
+        updateIcon();
+        emit checked(ischecked);
+    } else if(event->button() == Qt::RightButton) {
+        emit clicked_right();
+    }
+}
+
+void IconButton::updateIcon() {
+    if(ischecked) {
+        setIcon(pixmap_checked);
+    }
+    else {
+        setIcon(pixmap_unchecked);
+    }
+}
+
+void IconButton::setChecked(bool check) {
+    ischecked = check;
+    updateIcon();
+}
+
+void IconButton::setCheckIcon(QIcon checkedIcon, QIcon uncheckedIcon) {
+    pixmap_checked = checkedIcon;
+    pixmap_unchecked = uncheckedIcon;
+}
+
+bool IconButton::getIsChecked() {
+    return ischecked;
+}
---
+++ iconbutton.h
@@ -0,0 +1,32 @@
+﻿#ifndef IconButton_H
+#define IconButton_H
+
+#include <QPushButton>
+#include <QIcon>
+#include <QMouseEvent>
+
+class IconButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    IconButton(QWidget *parent = nullptr);
+    ~IconButton();
+
+    void setChecked(bool check);
+    void setCheckIcon(QIcon checkedIcon, QIcon uncheckedIcon);
+    void updateIcon();
+    bool getIsChecked();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+
+private:
+    bool ischecked;
+    QIcon pixmap_checked;
+    QIcon pixmap_unchecked;
+signals:
+    void checked(bool check);
+    void clicked_right();
+};
+
+#endif // IconButton_H
